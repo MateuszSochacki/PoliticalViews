22e8591fb5db366c451024fbabd3c5eb77f88f1a
Mat <msochacki5@gmail.com>
2017-10-11 21:27:49 +0200

    initial commit, react + spring boot

---
+++ src/main/java/com/react/reacttest/ReacttestApplication.java
@@ -0,0 +1,12 @@
+package com.react.reacttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReacttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReacttestApplication.class, args);
+	}
+}
---
+++ src/main/js/app.js
@@ -0,0 +1,7 @@
+"use-strict"
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Home = require('./home');
+
+ReactDOM.render(React.createElement(Home, null), document.getElementById("app"));
