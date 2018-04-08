9bf03e12489b621f52c7cdfb3135d63e686b892e
Mat <msochacki5@gmail.com>
2018-04-08 18:06:39 +0200

    Form controls, test questions

--- src/main/js/components/Economy.jsx
+++ src/main/js/components/Economy.jsx
@@ -8,6 +8,7 @@ import {
 } from 'material-ui/Form';
 import Radio, { RadioGroup } from 'material-ui/Radio';
 import Typography from "material-ui/Typography";
+import {Checkbox} from "material-ui";
 
 function FormContainer(props) {
     return (
@@ -18,7 +19,9 @@ function FormContainer(props) {
 }
 
 function getQuestion(num) {
-    const questions = [];
+    const questions = ["Jaki typ opodatkowania od osób fizycznych powinień obowiązywać?",
+    "Czy uważasz, że osoby bogate powinny zostać dodatkowo opodatkowane?",
+    ""];
     return (questions[num]);
 }
 
@@ -46,6 +49,40 @@ class Economy extends Component {
         };
     }
 
+    getRadioGroup(text1, text2, text3, text4, name, groupValue) {
+
+        return (
+            <RadioGroup
+                aria-label={name}
+                name={name}
+                value={groupValue}
+                onChange={this.handleChangeRadio}
+                row={true}
+            >
+                <FormControlLabel value={text1} control={<Radio/>} label={text1}/>
+                <FormControlLabel value={text2} control={<Radio/>} label={text2}/>
+                <FormControlLabel value={text3} control={<Radio/>} label={text3}/>
+                <FormControlLabel value={text4} control={<Radio/>} label={text4}/>
+            </RadioGroup>
+        )
+    }
+
+    getCheckboxForm(val, label, checkVal) {
+
+        return (
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={checkVal}
+                        onChange={this.handleChangeCheck}
+                        value={val}
+                    />
+                }
+                label={label}
+            />
+        );
+    }
+
     saveData() {
         fetch('http://localhost:8080/api/economy',
             {   headers: {
@@ -66,90 +103,92 @@ class Economy extends Component {
         return(
             <div>
                 <Typography align="center">
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                    <FormControl component="fieldset" required>
+                        <FormContainer>
+                            <FormLabel component="legend">{getQuestion(0)}</FormLabel>
+                            {this.getRadioGroup("Brak podatku", "Podatek pogłówny",
+                                "Podatek liniowy", "Podatek progresywny", "incTax", this.state.values.incTax)}
+                        </FormContainer>
+                    </FormControl><br/>
+                    <FormControl component="fieldset" required>
+                        <FormContainer>
+                            <FormLabel component="legend">{getQuestion(1)}</FormLabel>
+                            {this.getRadioGroup("Wszystkich powinny obowiązywać te same zasady", "Dodatkowy podatek na służby publiczne, lub wyższy próg PIT",
+                                "Dochód nie powinien przekraczać pewnej kwoty", "Nie powinno być ludzi zbyt bogatych", "rich", this.state.values.rich)}
+                        </FormContainer>
+                    </FormControl><br/>
+                    <FormControl component="fieldset" required>
+                        <FormContainer>
+                            <FormLabel component="legend">{getQuestion(2)}</FormLabel>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </FormContainer>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
+                        </RadioGroup>
+                    </FormControl>
+                    <FormControl component="fieldset" required>
+                        <FormLabel component="legend"></FormLabel>
+                        <RadioGroup>
 
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
-
-                    </RadioGroup>
-                </FormControl>
-                <FormControl component="fieldset" required>
-                    <FormLabel component="legend"></FormLabel>
-                    <RadioGroup>
-
-                    </RadioGroup>
-                </FormControl>
+                        </RadioGroup>
+                    </FormControl>
                 </Typography>
             </div>
         );
